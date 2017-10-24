6ad8687bde5d019789529ce4aa29aba8fec864df
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-10-24 15:19:51 +0500

    add eslint, editor config, bootstrap theme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nw.js-app-template
+
 A simple template for developing application using NW.js
---
+++ js/script.js
@@ -0,0 +1,2 @@
+// Mixing js and Node.js code in the same file? Yes please!
+alert('Rock on!!! \\(-_- )/')
